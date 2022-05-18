3399ead17abe7bbb0e409281486e7afd96cf0275
gvv70 <goyvv70@gmail.com>
2022-05-18 15:27:19 +0300

    Goy Vitaliy. All methods are implemented.

--- src/org/way2it/data_structures/binary_tree/BinaryTree.java
+++ src/org/way2it/data_structures/binary_tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package org.way2it.data_structures.binary_tree;
 
+import org.way2it.data_structures.linked_list.LinkedList;
+
 public class BinaryTree {
 
     // Represents the first node of this tree
@@ -15,20 +17,110 @@ public class BinaryTree {
     // Values that are less than or equal to the value in the current node, should be placed in the left subtree
     // Values that are greater than the value in the current node - should be placed in the right subtree
     public void add(int value) {
+        Node node = new Node();
+        Node currentNode = root;
+        boolean endNode = false;
+        if (root == null){
+            root = node;
+            root.value = value;
+            root.deleted = false;
+            size++;
+        }
+        else {
+            while (!endNode) {
+                if (value <= currentNode.value) {
+                    if (currentNode.left == null) {
+                        node.value = value;
+                        currentNode.left = node;
+                        currentNode.deleted = false;
+                        size++;
+                        endNode = true;
+                    }
+                    else {
+                        currentNode = currentNode.left;
+                    }
+                } else {
+                    if (currentNode.right == null) {
+                        node.value = value;
+                        currentNode.right = node;
+                        currentNode.deleted = false;
+                        size++;
+                        endNode = true;
+                    }
+                    currentNode = currentNode.right;
+                }
+
+            }
+        }
         // TODO implement me
     }
 
     // Should remove specified value from tree and return true
     // If value does not exist in this tree - return false
     public boolean remove(int value) {
+        Node currentNode = root;
+        currentNode.value = root.value;
+        boolean out = false;
+        while (currentNode != null) {
+            if (value == currentNode.value) {
+                out = true;
+                currentNode.deleted = true;
+                size--;
+                break;
+            } else if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+        /*Node currentNode = root;
+        Node rootTemp = null;
+        Node currentTemp = rootTemp;
+        boolean out = false;
+
+        if (root.value == value){
+            if (currentNode.right != null){
+                root = currentNode.right;
+                rootTemp = currentNode.left;
+            }
+            else if (currentNode.left !=null){
+                root = currentNode.left;
+            }
+            else {
+                root = null;
+            }
+            //System.out.println("root.value = " + root.value);
+            //System.out.println("rootTemp.value = " + rootTemp.value);
+            out = true;
+            size--;
+        }
+        else if (root.value < value){
+
+        }*/
+
         // TODO implement me
-        return false;
+        return out;
     }
 
     // Should return true if this tree contains specified value, false - otherwise
     public boolean contains(int value) {
+        Node currentNode = root;
+        currentNode.value = root.value;
+        boolean out = false;
+        while (currentNode != null) {
+            if (value == currentNode.value) {
+                if (currentNode.deleted == false) {
+                    out = true;
+                }
+                break;
+            } else if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
         // TODO implement me
-        return false;
+        return out;
     }
 
     public int getSize() {
@@ -39,5 +131,6 @@ public class BinaryTree {
         int value;
         Node left;
         Node right;
+        boolean deleted;
     }
 }
